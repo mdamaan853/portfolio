0bc6d27a5d08f42af887b44141e48cddf551280f
mdamaan853 <mdamaan853@gmail.com>
2021-12-15 00:20:48 +0530

    initial

---
+++ src/resumeData.js
@@ -0,0 +1,136 @@
+let resumeData = {
+  "imagebaseurl": "https://rbhatia46.github.io/",
+  "name": "Md Amaan",
+  "role": "Fullstack Developer",
+  "linkedinId": "https://www.linkedin.com/in/md-amaan/",
+  "skypeid": "https://join.skype.com/invite/WdiujXBfs0fl",
+  "roleDescription": "I like dabbling in various parts of Fullstack development and like to learn about new technologies,simply play games in my free time.",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "https://www.linkedin.com/in/md-amaan/",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "http://github.com/mdamaan853",
+      "className": "fa fa-github"
+    },
+    {
+      "name": "skype",
+      "url": "https://join.skype.com/invite/WdiujXBfs0fl",
+      "className": "fa fa-skype"
+    }
+  ],
+  "aboutme": "I am currently a pre-final year student at Arka Jain University and pursuing my B.Tech from here. I am a self taught Full Stack Web Developer, currently diving into Data Structures and . I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+  "address": "India",
+  "website": "https://rbhatia46.github.io",
+  "education": [
+    {
+      "UniversityName": "Arka jain University",
+      "specialization": "Bachelors of Technology in Computer Science",
+      "MonthOfPassing": "Aug",
+      "YearOfPassing": "2023 expected",
+      // "Achievements":"Some Achievements"
+    },
+    {
+      "UniversityName": "R D TATA Technical Education Centre - NTTF",
+      "specialization": "Diploma in Computer engineering and cloud computing",
+      "MonthOfPassing": "July",
+      "YearOfPassing": "2020",
+      "Achievements": "First prize in Project competition"
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "DACHRS (OPC) PRIVATE LIMITED ",
+      "specialization": "FRONTEND DEVELOPER",
+      "MonthOfJoining": "July",
+      "MonthOfLeaving": "september",
+      "YearOfLeaving": "2021",
+      // "Achievements":"Some Achievements"
+    },
+    {
+      "CompanyName": "MUSOCIAL PRIVATE LIMITED",
+      "specialization": "FULL STACK DEVELOPER INTERN",
+      "MonthOfJoining": "March",
+      "MonthOfLeaving": "June",
+      "YearOfLeaving": "2021",
+      // "Achievements":"Some Achievements"
+    },
+    {
+      "CompanyName": "RECA SOLUTION PRIVATE LIMITED",
+      "specialization": "SOFTWARE DEVELOPER INTERN",
+      "MonthOfJoining": "September",
+      "MonthOfLeaving": "December",
+      "YearOfLeaving": "2020",
+      // "Achievements":"Some Achievements"
+    }
+  ],
+  "skillsDescription": "Your skills here",
+  "skills": [
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "JS"
+    },
+    {
+      "skillname": "React"
+    },
+    {
+      "skillname": "Node"
+    },
+    {
+      "skillname": "Express"
+    },
+    {
+      "skillname": "C"
+    },
+    {
+      "skillname": "JAVA"
+    },
+    {
+      "skillname": "Mysql"
+    }, {
+      "skillname": "MongoDB"
+    },
+  ],
+  // "portfolio": [
+  //   {
+  //     "name": "project1",
+  //     "description": "mobileapp",
+  //     "imgurl": "images/portfolio/phone.jpg"
+  //   },
+  //   {
+  //     "name": "Treevesto",
+  //     "description": "A multi-vendor Ecommerce web site ",
+  //     "imgurl": "images/portfolio/project.jpg"
+  //   },
+  //   {
+  //     "name": "project3",
+  //     "description": "mobileapp",
+  //     "imgurl": "images/portfolio/project2.png"
+  //   },
+  //   {
+  //     "name": "project4",
+  //     "description": "mobileapp",
+  //     "imgurl": "images/portfolio/phone.jpg"
+  //   }
+  // ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
