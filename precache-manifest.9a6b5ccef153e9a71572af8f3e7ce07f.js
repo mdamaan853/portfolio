fa03ffd76dd06f1174bba6353fef8096de6f81bf
mdamaan853 <mdamaan853@gmail.com>
2021-12-15 21:24:10 +0530

    Updates

---
+++ precache-manifest.9a6b5ccef153e9a71572af8f3e7ce07f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb31071627b3cd180ba23b75c3286827",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a7a03574cbce76fee56d",
+    "url": "/portfolio/static/css/main.e8c24495.chunk.css"
+  },
+  {
+    "revision": "fd1a25ae27544c207377",
+    "url": "/portfolio/static/js/2.fa09f7e7.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/portfolio/static/js/2.fa09f7e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7a03574cbce76fee56d",
+    "url": "/portfolio/static/js/main.b0df4418.chunk.js"
+  },
+  {
+    "revision": "6bafe6fdb4893a0fd8e5",
+    "url": "/portfolio/static/js/runtime-main.459797f6.js"
+  },
+  {
+    "revision": "a2fb07a889e971def9e785834cb10ac2",
+    "url": "/portfolio/static/media/Amaan_resume.a2fb07a8.pdf"
+  }
+]);
